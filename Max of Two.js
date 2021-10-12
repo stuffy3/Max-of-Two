685dc985e45ccce8a52bf226f79708490f588d85
Tanner Stufflebeam <65130368+stuffy3@users.noreply.github.com>
2021-10-12 09:26:08 -0600

    Add files via upload

---
+++ Max of Two.js
@@ -0,0 +1,21 @@
+/*Given the starting code of:
+
+x = 1
+y = 2
+Write a block of code that will print out the largest value.
+
+Usnig the given values as an example, your code should print out 2.
+
+To verify your code is working properly, try and change the values of x and y.
+
+As an additional challenge, print out The values are identical. if x and y have the same value.*/
+
+let x = 2
+let y = 2
+if(x > y) {
+  console.log(x);
+} else if(x < y) {
+  console.log(y)
+} else if(x === y)
+  {console.log("fart")
+  };
